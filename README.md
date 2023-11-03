48e34f4290f169d49e55982e98c717e5143259e2
Meng Ye <meng.ye@sri.com>
2023-11-03 10:27:14 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # sri-ta2-mappable-criteria
+
+## Dependencies
+- PyMuPDF
+- Pytorch
+- Huggingface
+- Llama-Index
+- PyYAML
+- ...
+
+## Config
+check `base_config.yaml`
+
+## Quick start
+```
+python main.py base_config.yaml
+```
+
+## Usage
+
+1. text extraction from pdf
+```
+python text_extraction.py --config cfg_file_name.yaml
+```
+
+2. generate prompts for identifying mineral system components
+```
+python generate_prompts_mineral.py --config cfg_file_name.yaml
+```
+
+3. run LLM queries (OpenAI API)
+```
+python run_completion.py --input_dir /path/to/input/prompts --output_dir /path/to/output/response
+```
+
+4. aggregate response into mappable criteria prompts
+```
+python generate_prompts_mappable.py --config cfg_file_name.yaml
+```
+
+5. run LLM queries (OpenAI API)
+```
+python run_completion.py --input_dir /path/to/input/prompts --output_dir /path/to/output/response
+```
+
+6. generate output schema
+```
+python output_schema.py --config cfg_file_name.yaml
+```
+
+## TODO
+- output schema in .json not .txt
+- add bounding boxes and page number
+- visualize output on pdf
+- create dockerfile
+- ...
---
+++ generate_prompts_mineral.py
@@ -0,0 +1,57 @@
+import os
+import json
+import yaml
+import random
+import argparse
+from collections import OrderedDict
+
+
+def generate_requests(doc, config, sample_per_doc=0):
+    if sample_per_doc > 0:
+        nodes = random.sample(nodes, k=min(sample_per_doc, len(nodes)))
+
+    requests = OrderedDict()
+    for node_id, node in doc.items():
+        message_user = config["templates"]["user"].format(context_str = node["text"].replace('\n', ' '))
+        request = {
+                "model": config["llm_model"],
+                "messages": [
+                    {"role": "system", "content": config["templates"]["system"]},
+                    {"role": "user", "content": message_user},
+                ],
+                "temperature": config["llm_temperature"],
+                "metadata": {
+                    "node_id": node_id,
+                }
+            }
+        requests[node_id] = request
+    return requests
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, default='base_config.yaml')
+    args = parser.parse_args()
+
+    with open(args.config) as f:
+        cfg = yaml.load(f, Loader=yaml.FullLoader)
+    cfg_ = cfg['mineral_system']
+
+    doc_dir = os.path.join(cfg['log_dir'], cfg['text_extraction']['doc_dir'])
+    out_dir = os.path.join(cfg['log_dir'], cfg_['prompts_dir'])
+    os.makedirs(out_dir, exist_ok=True)
+
+    llm_config = cfg_['llm_config']
+    file_list = [f for f in os.listdir(doc_dir)]
+
+    for doc_fname in file_list:
+        doc_fname_full = os.path.join(doc_dir, doc_fname)
+        with open(doc_fname_full, 'r') as f:
+            doc = json.load(f, object_pairs_hook=OrderedDict)
+        requests = generate_requests(doc, llm_config)
+
+        request_fname = os.path.join(out_dir, doc_fname.replace('.json', '.jsonl'))
+        f = open(request_fname, 'w')
+        for node_id, req in requests.items():
+            f.write(json.dumps(req) + '\n')
+        f.close()
