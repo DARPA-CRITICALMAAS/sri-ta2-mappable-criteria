04b1304a82f6cbe62e6e967d71abd1f3c0de29d7
Meng Ye <meng.ye@sri.com>
2024-04-10 15:19:57 -0400

    update docker image name

--- docker_run.sh
+++ docker_run.sh
@@ -3,5 +3,5 @@ docker run -it \
 -v /data/meng/datalake/cmaas-ta2/k8s/meng/mappable_criteria/sri-ta2-mappable-criteria/logs:/workdir/logs \
 -v /data/meng/datalake/cmaas-ta2/k8s/meng/mappable_criteria/sri-ta2-mappable-criteria/data:/workdir/data \
 -e OPENAI_API_KEY=$OPENAI_API_KEY \
-cmaas-ta2-sri-mappable \
+cmaas-ta2-sri-mappable-criteria \
 python main.py configs/new_config_nickel.yaml
