50538a292c6219b35d8c1976d1baca9f2736af40
Meng Ye <meng.ye@sri.com>
2024-07-30 15:00:23 -0400

    update command

--- polygon_ranking/docker_run.sh
+++ polygon_ranking/docker_run.sh
@@ -2,15 +2,15 @@ docker run --rm -it \
     --gpus all \
     -v /data/meng/datalake/cmaas-ta2/k8s/meng/mappable_criteria/geoscience_language_models/project_tools/notebooks/SGMC/USGS_SGMC_Shapefiles/SGMC_Geology.dbf:/workdir/SGMC_Geology.dbf \
     -v /data/meng/datalake/cmaas-ta2/k8s/meng/mappable_criteria/geoscience_language_models/project_tools/notebooks/SGMC/USGS_SGMC_Tables_CSV/SGMC_Units.csv:/workdir/SGMC_Units.csv \
-    -v /home/meng/Downloads/test/output_preproc:/workdir/output_preproc \
+    -v /home/meng/Downloads/12_month_hack/preproc:/workdir/output_preproc \
     --entrypoint sh \
     sri-map-synth \
-    -c 'python polygon_ranking.py preproc --input_shapefile SGMC_Geology.dbf --input_desc SGMC_Units.csv --output output_preproc/SGMC_preproc.parquet'
+    -c 'python polygon_ranking.py preproc --input_shapefile SGMC_Geology.dbf --input_desc SGMC_Units.csv --output output_preproc/SGMC_preproc_v1.parquet'
 
 docker run --rm -it \
     --gpus all \
-    -v /data/meng/datalake/cmaas-ta2/k8s/meng/mappable_criteria/sri-ta2-mappable-criteria/polygon_ranking/output_preproc:/workdir/output_preproc \
-    -v /home/meng/Downloads/12_month_hack/July_2:/workdir/output_rank \
+    -v /home/meng/Downloads/12_month_hack/preproc:/workdir/output_preproc \
+    -v /home/meng/Downloads/12_month_hack/July_30/iaross_cmbert:/workdir/output_rank \
     --entrypoint sh \
     sri-map-synth \
-    -c 'python polygon_ranking.py rank --processed_input output_preproc/SGMC_preproc.parquet --deposit_type mvt_zinc_lead hectorite_li --normalize --output_dir output_rank'
+    -c 'python polygon_ranking.py rank --processed_input output_preproc/SGMC_preproc_v1.parquet --deposit_type mvt_zinc_lead hectorite_li --hf_model iaross/cm_bert --normalize --output_dir output_rank'
