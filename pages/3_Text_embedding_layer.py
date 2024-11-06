75d792b6fa2550c3df0de4c41bf423e7da331866
Meng Ye <meng.ye@sri.com>
2024-11-06 10:05:15 -0500

    merge results into dep-model pipeline

--- pages/3_Text_embedding_layer.py
+++ pages/3_Text_embedding_layer.py
@@ -238,7 +238,7 @@ with tab1:
                     if download:
                         zip_buffer = save_df2zipgeojson(gpd_data_filtered, 'data')
                         st.download_button(
-                            label=f"download data as zip",
+                            label=f"Zipped GeoJson",
                             data=zip_buffer,
                             file_name=f"data.zip",
                             mime="application/zip",
@@ -291,6 +291,8 @@ with tab2:
                     pd.DataFrame([{'characteristic':k, 'description':v} for k, v in dep_model.items()]),
                     hide_index=True,
                 )
+
+                selected_characteristics = []
             else:
                 st.warning("Please select a deposit type")
         else:
@@ -343,6 +345,7 @@ with tab2:
         parser.add_argument('--hf_model', type=str, default='iaross/cm_bert')
         parser.add_argument('--deposit_models', type=str, default='deposit_models/deposit_models.json')
         parser.add_argument('--deposit_type', type=str, nargs='+', default=[])
+        parser.add_argument('--characteristics', type=str, nargs='+', default=[])
         parser.add_argument('--negatives', type=str, default=None)
         parser.add_argument('--normalize', action='store_true', default=False)
         parser.add_argument('--boundary', type=nullable_string, default=None)
@@ -369,6 +372,7 @@ with tab2:
             "--desc_col", desc_col,
             "--deposit_models", selected_dep_model_file,
             "--deposit_type", selected_dep_type,
+            "--characteristics", selected_characteristics,
             "--hf_model", selected_embedding_model,
             "--output_dir", output_dir,
             "--version", "v1.1",
@@ -402,3 +406,89 @@ with tab2:
             rank(parser.parse_args(msg))
         else:
             st.warning(msg)
+    
+
+    st.write("### Results")
+    map_dir = st.session_state['text_emb_layers']
+
+
+    results = [d for d in os.listdir(map_dir) if os.path.isdir(os.path.join(map_dir, d))]
+
+    selected_dir = st.selectbox(
+        "choose a finished job",
+        results,
+        key='tab3.results'
+    )
+
+    if selected_dir:
+        cmas = os.listdir(os.path.join(map_dir, selected_dir))
+        cmas = list(set([f.replace('.gpkg', '').replace('.raster', '') for f in cmas]))
+
+        selected_cma = st.selectbox(
+            "choose a cma",
+            cmas,
+            key='tab3.cma'
+        )
+
+        cma = os.path.join(map_dir, selected_dir, selected_cma)
+        cma_raster_dir = cma + '.raster'
+        layers = list(set([f.split('.')[0] for f in os.listdir(cma_raster_dir)]))
+    else:
+        layers = []
+
+    color_maps = ["Blues", "Greens", "Oranges", "Reds", "Purples"]
+    m = leafmap.Map(
+        center=(38, -100),
+        tiles='Cartodb Positron',
+        zoom=4,
+        max_zoom=20,
+        min_zoom=2,
+        height=800
+    )
+
+    for i, l in enumerate(layers):
+        # m.add_raster(os.path.join(cma_raster_dir, l+'.tif'), colormap=color_maps[i%len(color_maps)], layer_name=l)
+        m.add_raster(os.path.join(cma_raster_dir, l+'.tif'), colormap='plasma', layer_name=l)
+
+    m.to_streamlit()
+
+    with st.expander("Download"):
+        st.write("download geopackage/geojson")
+
+    with st.expander("Push to CDR"):
+        to_view = None
+        with st.container(height=400):
+            selected_options = []
+            select_all = st.checkbox("select all layers")
+
+            for i, l in enumerate(layers):
+                col1, col2 = st.columns(2)
+                with col1:
+                    if st.checkbox(l, value=select_all):
+                        selected_options.append(l)
+                with col2:
+                    if st.button("metadata", key=f'tab3.meta.{i}'):
+                        to_view = l
+
+        if to_view:
+            with open(os.path.join(cma_raster_dir, to_view+'.json'), 'r') as f:
+                metadata = json.load(f)
+            st.json(metadata, expanded=2)
+
+
+        cdr_key = st.text_input("Your CDR key:", type="password")
+        pushed = st.button("Push to CDR")
+        if pushed:
+            with st.container(height=200):
+                if not cdr_key:
+                    st.warning("please provide a CDR key")
+                else:
+                    for l in selected_options:
+                        metadata_fname = os.path.join(cma_raster_dir, l+'.json')
+                        tif_fname = os.path.join(cma_raster_dir, l+'.tif')
+
+                        with open(metadata_fname, 'r') as f:
+                            metadata = json.load(f)
+                        st.info(f'pushing {l} to CDR ...')
+                        response = push_to_cdr(cdr_key, metadata, tif_fname)
+                        st.info(response)
