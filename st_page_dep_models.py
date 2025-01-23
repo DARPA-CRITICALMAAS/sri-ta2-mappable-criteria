b5ab0c374d6fc29309da5c3c7463e07cfb46ca99
Meng Ye <meng.ye@sri.com>
2025-01-22 21:32:31 -0500

    upload/download deposit models

--- st_page_dep_models.py
+++ st_page_dep_models.py
@@ -21,16 +21,16 @@ def df2dict(df, key='characteristic', value='description'):
     }
     return dict
 
-@st.dialog(title="Load file", width="large")
+@st.dialog(title="Load file and edit", width="large")
 def load_dep_models():
-    files = [fname for fname in os.listdir(deposit_model_dir) if fname.endswith('.json')]
+    files = [fname for fname in os.listdir(deposit_model_dir) if '.json' in fname]
     files.sort()
     dep_model_file = st.selectbox(
         'choose a file',
         files,
         index=None,
     )
-    if st.button("load", icon=":material/restart_alt:"):
+    if st.button("load & edit", icon=":material/restart_alt:"):
         if dep_model_file:
             full_fname = os.path.join(deposit_model_dir, dep_model_file)
             with open(full_fname, 'r') as f:
@@ -45,7 +45,7 @@ def load_dep_models():
 
 @st.dialog(title="Delete file", width="large")
 def delete_dep_models():
-    files = [fname for fname in os.listdir(deposit_model_dir) if fname.endswith('.json')]
+    files = [fname for fname in os.listdir(deposit_model_dir) if '.json' in fname]
     files.sort()
     dep_model_file = st.selectbox(
         'choose a file',
@@ -60,6 +60,56 @@ def delete_dep_models():
             st.rerun()
         else:
             st.warning("you have not choosen a deposit model file")
+
+@st.dialog(title="Download deposit models", width="large")
+def download_dep_models():
+    files = [fname for fname in os.listdir(deposit_model_dir) if '.json' in fname]
+    files.sort()
+    dep_model_file = st.selectbox(
+        'choose a file',
+        files,
+        index=None,
+    )
+    if dep_model_file:
+        full_fname = os.path.join(deposit_model_dir, dep_model_file)
+        with open(full_fname, 'r') as f:
+            json_str = json.dumps(json.load(f))
+
+        st.download_button(
+            label="Download",
+            icon=":material/download:",
+            file_name=dep_model_file,
+            mime="application/json",
+            data=json_str,
+        )
+
+@st.dialog(title="Upload deposit models", width="large")
+def upload_dep_models():
+    uploaded_files = st.file_uploader(
+        "Upload .json files:", accept_multiple_files=True
+    )
+    for uploaded_file in uploaded_files:
+        if uploaded_file is not None:
+            # Get the original file name
+            original_file_name = uploaded_file.name
+
+            # Determine a unique file name
+            save_path = os.path.join(deposit_model_dir, original_file_name)
+            base_name, extension = os.path.splitext(original_file_name)
+            counter = 1
+
+            while os.path.exists(save_path):
+                # Generate a new file name if it already exists
+                new_file_name = f"{base_name}_{counter}{extension}"
+                save_path = os.path.join(deposit_model_dir, new_file_name)
+                counter += 1
+
+            # Save the uploaded file
+            with open(save_path, "wb") as file:
+                file.write(uploaded_file.read())
+
+            st.success(f"File uploaded successfully as: {os.path.basename(save_path)}")
+
 
 @st.dialog(title="Create new deposit type", width="large")
 def add_new_dep_type():
@@ -91,11 +141,15 @@ def save_to_new():
 
 deposit_model_dir = st.session_state['deposit_model_dir']
 
-cols = st.columns(4)
+cols = st.columns([1,1,1,1,4])
 with cols[0]:
-    st.button("load deposit models", icon=":material/restart_alt:", type="primary", on_click=load_dep_models)
+    st.button("edit", icon=":material/edit:", type="primary", on_click=load_dep_models, use_container_width=True)
 with cols[1]:
-    st.button("delete deposit models", icon=":material/delete:", on_click=delete_dep_models)
+    st.button("delete", icon=":material/delete:", on_click=delete_dep_models, use_container_width=True)
+with cols[2]:
+    st.button("download", icon=":material/download:", on_click=download_dep_models, use_container_width=True)
+with cols[3]:
+    st.button("upload", icon=":material/upload:", on_click=upload_dep_models, use_container_width=True)
 
 if 'selected_dep_model_file' in st.session_state:
     st.info(f"loaded file **'{st.session_state['selected_dep_model_file']}'**")
